8f144d86dad828766fb737f5089078cf112f47e9
Naga Vijayapuram <naga_vijayapuram@gap.com>
2014-12-25 20:28:44 -0800

    simple hello world scripts

---
+++ hw2.scala
@@ -0,0 +1,18 @@
+// -----------------------------------
+//
+// Running -
+//
+// $ scalac hw2.scala && scala hw2
+//
+// Or -
+//
+// $ fsc hw2.scala && scala hw2
+//
+// -----------------------------------
+
+object hw2 extends App {
+
+    println("Hello World!")
+
+}
+
