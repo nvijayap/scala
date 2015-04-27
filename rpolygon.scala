69a51f8b8ecbfe46608c5acc4e7e874d022e28ea
Naga Vijayapuram <naga_vijayapuram@gap.com>
2015-04-27 14:05:33 -0700

    Added Helpful Comments

--- rpolygon.scala
+++ rpolygon.scala
@@ -12,11 +12,11 @@ def rpolygon(n:Int, s:Int) = { // n: # of sides, s: length of side
   (n * s, 0.25 * n * s * s / math.tan(math.Pi/n))
 }
 
-val (circumference, area) = rpolygon(4,3)
+val (circumference, area) = rpolygon(4,3) // regular polygon of 4 sides, each side = 3 units
 println("=> Circumference: " + circumference)
 println("=> Area: " + area)
 
-val (circumference2, area2) = rpolygon(5,3)
+val (circumference2, area2) = rpolygon(5,3) // regular polygon of 5 sides, each side = 3 units
 println("=> Circumference: " + circumference2)
 println("=> Area: " + area2)
 
